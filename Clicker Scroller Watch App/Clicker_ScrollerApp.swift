701485626505d6de6c9596e07e125ffb2fbc0231
stanlyvm1 <satana2026@gmail.com>
2026-06-14 22:49:54 -0700

    Initial Commit

---
+++ Clicker Scroller Watch App/Clicker_ScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clicker_ScrollerApp.swift
+//  Clicker Scroller Watch App
+//
+//  Created by Станіслав Стреляний on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Clicker_Scroller_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clicker Scroller Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clicker Scroller Watch App
+//
+//  Created by Станіслав Стреляний on 14.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
